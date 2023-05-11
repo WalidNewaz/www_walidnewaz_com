cc0a1bb91415ad45bb0fb61f87beee77d9d00319
Walid Newaz <mdwalid@gmail.com>
2023-05-10 22:47:32 -0600

    Added page templates for the blogs category and blog post pages.

---
+++ src/pages/blog/{postedDate}{mdx.frontmatter__slug}.tsx
@@ -0,0 +1,5 @@
+/**
+ * This file contains all logic resposible for rendering a
+ * blog page. The URL path to this page should be as follows:
+ * /blog/yyyy/mm/dd/post-slug
+ */
---
+++ src/pages/blog/{topic}.tsx
@@ -0,0 +1,9 @@
+/**
+ * This file contains all logic to render individual pages
+ * that filter based on the selected topic. The URL path
+ * to this page follows the following pattern:
+ * 
+ * /blog/{topic}
+ * 
+ * Currently there is no pagination support. 
+ */
